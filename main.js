641fcb21870d51fea7fcd824202a2573a6f675cc
sekizawakeisuke <sekizawakeisuke@users.noreply.github.com>
2016-02-20 16:03:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,12 +15,13 @@ app.use(parser.urlenclosed({ extended: false}));
 app.use(parser.json());
 app.use(express.static(__dirname + '/public'))
 
+/**
 app.get('/',function (req, res, next){
     res.json("ww");
     return next();
   });
 
-/**  
+
 app.get('/api/projects',function (req, res, next){
   knex.select('*').from('projects')
   .then(function (projects){
